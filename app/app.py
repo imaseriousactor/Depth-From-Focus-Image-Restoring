bb2fe689658dcc5dfb754f68392ce7e8a4fc4f0b
Maria Ovsiannikova <maovsiannikova@edu.hse.ru>
2026-06-10 08:55:17 +0300

    Add configs, app, evaluate script and project structure

---
+++ app/app.py
@@ -0,0 +1,9 @@
+﻿import gradio as gr
+import torch
+from models.attention_unet import AttentionUNet
+
+def predict(image):
+    return image
+
+demo = gr.Interface(fn=predict, inputs="image", outputs="image")
+demo.launch()
---
+++ evaluate.py
@@ -0,0 +1,6 @@
+﻿import torch
+from models.unet import UNet
+from models.attention_unet import AttentionUNet
+from models.vit_hybrid import ViTHybrid
+
+print("Evaluate script ready")
